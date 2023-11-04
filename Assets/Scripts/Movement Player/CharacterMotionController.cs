4463b2dc7c09b8a037ab30ead7a2f6160acea084
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2023-11-04 15:45:56 +0700

    add movement, prefabs and new input system

---
+++ Assets/Scripts/Movement Player/CharacterMotionController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMotionController : MonoBehaviour
+{
+    [SerializeField] private InputAction _moveInputAction;
+    [SerializeField, Range(0, 100)] float _moveSpeed;
+    Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        _moveInputAction.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 input = _moveInputAction.ReadValue<Vector2>();
+        _rigidbody.velocity = new Vector3(input.x, 0, input.y) * _moveSpeed;
+    }
+
+    private void OnDisable()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _moveInputAction.Disable();
+    }
+}
