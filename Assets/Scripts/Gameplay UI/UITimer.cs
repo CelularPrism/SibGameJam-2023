cc3d9a08e6f7450cd82d2770667a0510af753679
Styopa Groznov <groznov3@gmail.com>
2023-11-04 15:05:52 +0300

    UI timer

---
+++ Assets/Scripts/Gameplay UI/UITimer.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay_UI
+{
+    public class UITimer : MonoBehaviour
+    {
+        private TMP_Text _time;
+
+        private void Awake()
+        {
+            _time = GetComponentInChildren<TMP_Text>();
+        }
+
+        public void Set(string formatedTime) => _time.text = formatedTime;
+    }
+}
