194d1f79666431c89bfb25b47d8869ce62dfbd6d
Styopa Groznov <groznov3@gmail.com>
2023-11-05 22:31:06 +0300

    Update BaseCheeseInventory.cs

--- Assets/Scripts/Cheese/BaseCheeseInventory.cs
+++ Assets/Scripts/Cheese/BaseCheeseInventory.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using Assets.Scripts.Gameplay_UI;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class BaseCheeseInventory : MonoBehaviour, IItem
 {
@@ -10,12 +8,20 @@ public class BaseCheeseInventory : MonoBehaviour, IItem
     [SerializeField] private EndGameEvent eventor;
 
     private int _cheese = 0;
+    private CheeseCounter _ui;
+
+    private void Awake()
+    {
+        _ui = FindAnyObjectByType<CheeseCounter>();
+    }
 
     public void Use()
     {
         if (inventory != null)
         {
             _cheese += inventory.RemoveCheese();
+            _ui.Set(_cheese, maxCheese);
+
             if (_cheese >= maxCheese)
             {
                 GameEvents.Instance.Subscribe(GameEventType.Won, eventor.Win);
