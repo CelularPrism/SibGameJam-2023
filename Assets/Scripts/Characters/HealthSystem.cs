71de69a17df4ed4013d7fdb621c61c9af5a3a52a
Styopa Groznov <groznov3@gmail.com>
2023-11-05 22:03:09 +0300

    Update HealthSystem.cs

--- Assets/Scripts/Characters/HealthSystem.cs
+++ Assets/Scripts/Characters/HealthSystem.cs
@@ -1,36 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using Assets.Scripts.Gameplay_UI;
 using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    [SerializeField] private float _health;
-    [SerializeField] private float _maxHealth;
+    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
+    private HealthBar _healthBar;
 
     private ICharacter _character;
 
     private void Start()
     {
         _character = transform.GetComponent<ICharacter>();
+        _healthBar = FindAnyObjectByType<HealthBar>();
+        _healthBar.Set(_health);
     }
 
-    public void Damage(float value = 1)
+    public void Damage(int value = 1)
     {
         _health -= value;
+        _health = Mathf.Clamp(_health, 0, _maxHealth);
+        _healthBar.Set(_health);
 
         if (_health <= 0)
         {
             if (_character.IsDead == false)
             {
-                _health = 0;
                 _character.Dead();
             }
         }
     }
 
-    public void Heal(float value = 1)
+    public void Heal(int value = 1)
     {
         _health += value;
+        _healthBar.Set(_health);
+
         if (_health > _maxHealth)
         {
             _health = _maxHealth;
