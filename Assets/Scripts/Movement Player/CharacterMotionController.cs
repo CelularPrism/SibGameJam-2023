c92fdeb71f61fc8c832af86c583199c3d9220430
Styopa Groznov <groznov3@gmail.com>
2023-11-05 20:56:05 +0300

    Animations

--- Assets/Scripts/Movement Player/CharacterMotionController.cs
+++ Assets/Scripts/Movement Player/CharacterMotionController.cs
@@ -13,11 +13,15 @@ public class CharacterMotionController : MonoBehaviour
     private CinemachineOrbitalTransposer _orbitalTransposer;
     private bool _stopped = false;
     private float _rotationVelocity = 0.1f;
+    private Animator _animator;
+    private int _runAnimationHash = Animator.StringToHash("IsRun");
+    private int _cryAnimationHash = Animator.StringToHash("IsCry");
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _orbitalTransposer = _virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+        _animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -36,13 +40,17 @@ public class CharacterMotionController : MonoBehaviour
             Vector3 direction = new Vector3(input.x, 0, input.y).normalized;
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _virtualCamera.transform.eulerAngles.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _rotationVelocity, _rotationSmooth);
-            transform.rotation = Quaternion.Euler(Vector3.up * smoothAngle);
             direction = Quaternion.Euler(Vector3.up * targetAngle) * Vector3.forward;
+
+            if (input != Vector2.zero)
+                transform.rotation = Quaternion.Euler(Vector3.up * smoothAngle);
 
             if (input == Vector2.zero)
                 direction = Vector3.zero;
 
             _characterController.Move(_moveSpeed * Time.deltaTime * direction.normalized);
+            _animator.SetBool(_runAnimationHash, direction != Vector3.zero);
+            _animator.SetBool(_cryAnimationHash, Input.GetMouseButton(0) && direction == Vector3.zero);
         }
     }
 
