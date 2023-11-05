15caaa5fe08f05cbeea550bffc92c843b01cc9c0
Styopa Groznov <groznov3@gmail.com>
2023-11-05 21:38:39 +0300

    Update UseEnvironments.cs

--- Assets/Scripts/Movement Player/UseEnvironments.cs
+++ Assets/Scripts/Movement Player/UseEnvironments.cs
@@ -1,63 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
 
-[RequireComponent(typeof(SphereCollider))]
 public class UseEnvironments : MonoBehaviour
 {
-    [SerializeField] private MovementInput input;
-    [SerializeField] private LayerMask layerMask;
-
-    private SphereCollider _sphere;
-    private Collider[] _colliders;
-    private Collider _collider;
+    [SerializeField] private float _raduis;
+    private MovementInput _input;
+    private Collider[] _usables = new Collider[5];
     private bool _canUse = false;
-    private float _raduis;
 
     public bool CanUse() => _canUse;
 
     private void OnEnable()
     {
-        input.Player.Enable();
+        _input.Player.Enable();
     }
 
     private void OnDisable()
     {
-        input.Player.Disable();
+        _input.Player.Disable();
     }
 
     void Awake()
     {
-        input = new MovementInput();
-        input.Player.Use.performed += Use;
-        _sphere = GetComponent<SphereCollider>();
-        _raduis = _sphere.radius;
+        _input = new MovementInput();
+        _input.Player.Use.performed += Use;
     }
 
     private void FixedUpdate()
     {
-        _colliders = Physics.OverlapSphere(transform.position, _raduis, layerMask);
-        if (_colliders.Length > 0)
+        if (Physics.OverlapSphereNonAlloc(transform.position, _raduis, _usables, LayerMask.GetMask("Usable Item")) > 0)
         {
             _canUse = true;
-            _collider = _colliders[0];
         } else
         {
             _canUse = false;
-            _collider = null;
         }
     }
 
     private void Use(CallbackContext calbackContext)
     {
+        Debug.Log(_usables[0]);
+
         if (_canUse)
         {
-            var item = _collider.GetComponent<IItem>();
-            if (item != null)
-                item.Use();
+            Debug.Log(_usables[0].GetComponent<IItem>());
+            _usables[0].GetComponent<IItem>()?.Use();
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _raduis);
     }
 }
