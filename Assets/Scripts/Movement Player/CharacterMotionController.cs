9a5252a8cdcc4e7a43469aedd0a7d617985bfc92
Styopa Groznov <groznov3@gmail.com>
2023-11-05 19:16:41 +0300

    New character

--- Assets/Scripts/Movement Player/CharacterMotionController.cs
+++ Assets/Scripts/Movement Player/CharacterMotionController.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterMotionController : MonoBehaviour
 {
     [SerializeField] private InputAction _moveInputAction;
-    [SerializeField, Range(0, 100)] float _moveSpeed;
-    Rigidbody _rigidbody;
+    [SerializeField] float _moveSpeed;
+    [SerializeField] float _rotationSmooth;
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField] private int _cameraRotationSpeed = 300;
+    private CharacterController _characterController;
+    private CinemachineOrbitalTransposer _orbitalTransposer;
     private bool _stopped = false;
+    private float _rotationVelocity = 0.1f;
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
+        _orbitalTransposer = _virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
     }
 
     private void OnEnable()
@@ -20,23 +25,31 @@ public class CharacterMotionController : MonoBehaviour
         _moveInputAction.Enable();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if (!_stopped)
+        if (_orbitalTransposer)
+            _orbitalTransposer.m_XAxis.m_MaxSpeed = Input.GetMouseButton(1) ? _cameraRotationSpeed : 0;
+
+        if (_stopped == false)
         {
             Vector2 input = _moveInputAction.ReadValue<Vector2>();
-            _rigidbody.velocity = new Vector3(input.x, 0, input.y) * _moveSpeed;
+            Vector3 direction = new Vector3(input.x, 0, input.y).normalized;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _virtualCamera.transform.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _rotationVelocity, _rotationSmooth);
+            transform.rotation = Quaternion.Euler(Vector3.up * smoothAngle);
+            direction = Quaternion.Euler(Vector3.up * targetAngle) * Vector3.forward;
+
+            if (input == Vector2.zero)
+                direction = Vector3.zero;
+
+            _characterController.Move(_moveSpeed * Time.deltaTime * direction.normalized);
         }
     }
 
     private void OnDisable()
     {
-        _rigidbody.velocity = Vector3.zero;
         _moveInputAction.Disable();
     }
 
-    public void Stop()
-    {
-        _stopped = true;
-    }
+    public void Stop() => _stopped = true;
 }
