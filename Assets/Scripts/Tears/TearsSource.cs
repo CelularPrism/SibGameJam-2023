dd6c0f598bcfd524dde9f4e41897e8f25afc1a5b
Styopa Groznov <groznov3@gmail.com>
2023-11-05 11:22:04 +0300

    Tears casting

---
+++ Assets/Scripts/Tears/TearsSource.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Tears
+{
+    public class TearsSource : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _leftEyeParticleSystem, _rightEyeParticleSystem;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                StartTearing();
+
+            if (Input.GetMouseButtonUp(0))
+                StopTearing();
+        }
+
+        private void StartTearing()
+        {
+            _leftEyeParticleSystem.Play();
+            _rightEyeParticleSystem.Play();
+        }
+
+        private void StopTearing()
+        {
+            _leftEyeParticleSystem.Stop();
+            _rightEyeParticleSystem.Stop();
+        }
+    }
+}
