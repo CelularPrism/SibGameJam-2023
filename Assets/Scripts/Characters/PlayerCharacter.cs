7ff5dd2e455390cc259e8837cf0aeb4c75031e35
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2023-11-06 13:47:09 +0700

    Update PlayerCharacter.cs

--- Assets/Scripts/Characters/PlayerCharacter.cs
+++ Assets/Scripts/Characters/PlayerCharacter.cs
@@ -5,10 +5,19 @@ using UnityEngine.TextCore.Text;
 
 public class PlayerCharacter : MonoBehaviour, ICharacter
 {
+    [SerializeField] private EndGameEvent eventor;
+
     public bool IsDead { get; private set; }
 
     public void Dead()
     {
+        GameEvents.Instance.Subscribe(GameEventType.Lose, eventor.Lose);
+        GameEvents.Instance.Dispatch(GameEventType.Lose);
         Debug.Log("Dead");
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.Instance.UnSubscribe(GameEventType.Lose, eventor.Lose);
     }
 }
