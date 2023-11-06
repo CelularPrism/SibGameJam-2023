95b972b41d0fc0f3f79fc6d1398c03ee9aa58773
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2023-11-06 09:00:47 +0700

    update

--- Assets/Scripts/Tears/TearsSource.cs
+++ Assets/Scripts/Tears/TearsSource.cs
@@ -1,13 +1,26 @@
-﻿using UnityEngine;
+﻿using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Assets.Scripts.Tears
 {
     public class TearsSource : MonoBehaviour
     {
         [SerializeField] private ParticleSystem _leftEyeParticleSystem, _rightEyeParticleSystem;
+        [SerializeField] private EventReference _event;
+        [SerializeField] private Transform _camera;
+
+        private EventInstance instance;
+
+        private void Start()
+        {
+            instance = RuntimeManager.CreateInstance(_event);
+        }
 
         private void Update()
         {
+            instance.set3DAttributes(RuntimeUtils.To3DAttributes(_camera.position));
             if (Input.GetMouseButtonDown(0))
                 StartTearing();
 
@@ -17,12 +30,17 @@ namespace Assets.Scripts.Tears
 
         private void StartTearing()
         {
+            instance.start();
+            instance.release();
+
             _leftEyeParticleSystem.Play();
             _rightEyeParticleSystem.Play();
         }
 
         private void StopTearing()
         {
+            instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
             _leftEyeParticleSystem.Stop();
             _rightEyeParticleSystem.Stop();
         }
