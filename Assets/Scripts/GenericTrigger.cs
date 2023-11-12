e8afa703d0d296ddbad3d62b0a0633e6bd5b94a9
Styopa Groznov <groznov3@gmail.com>
2023-11-12 19:03:59 +0300

    Generic trigger

---
+++ Assets/Scripts/GenericTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts
+{
+    public class GenericTrigger : MonoBehaviour
+    {
+        public UnityEvent OnEnter, OnStay, OnExit;
+
+        private void OnTriggerEnter(Collider other) => OnEnter?.Invoke();
+
+        private void OnCollisionEnter(Collision collision) => OnEnter?.Invoke();
+
+        private void OnTriggerStay(Collider other) => OnStay?.Invoke();
+
+        private void OnCollisionStay(Collision collision) => OnStay?.Invoke();
+
+        private void OnTriggerExit(Collider other) => OnExit?.Invoke();
+
+        private void OnCollisionExit(Collision collision) => OnExit?.Invoke();
+    }
+}
