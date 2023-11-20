5eb56eabe016a432b792b4f40d473aba8040b2c7
Styopa Groznov <groznov3@gmail.com>
2023-11-20 12:32:15 +0300

    Vignette pulsation

--- Assets/Scripts/Characters/VignetteController.cs
+++ Assets/Scripts/Characters/VignetteController.cs
@@ -2,15 +2,19 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace Assets.Scripts.Characters
 {
     public class VignetteController : MonoBehaviour
     {
         [SerializeField, Range(0, 1)] private float _limit;
+        [SerializeField] private AnimationCurve _pulsationCurve;
+        [SerializeField] private float _maxPulsationFrequency;
         private Vignette _vignette;
         private HealthSystem _health;
         private float _defaultVignetteIntens;
+        private float _time;
 
         private void Awake()
         {
@@ -22,11 +26,26 @@ namespace Assets.Scripts.Characters
 
         private void OnEnable() => _health.OnChange += OnHealthChanged;
 
+        private void Update()
+        {
+            float value = (_health.MaxHealth - _health.Health) / _health.MaxHealth;
+            float clampedValue = Mathf.Clamp(value, _defaultVignetteIntens, _limit);
+
+            if (_maxPulsationFrequency > 0)
+            {
+                if (_time > 1)
+                    _time = 0;
+
+                clampedValue += _pulsationCurve.Evaluate(_time);
+                _time += _maxPulsationFrequency * value * Time.deltaTime;
+            }
+
+            _vignette.intensity.value = clampedValue;
+        }
+
         private void OnHealthChanged(float value)
         {
-            value = (_health.MaxHealth - value) / _health.MaxHealth;
-            value = Mathf.Clamp(value, _defaultVignetteIntens, _limit);
-            _vignette.intensity.value = value;
+            
         }
 
         private void OnDisable() => _health.OnChange -= OnHealthChanged;
